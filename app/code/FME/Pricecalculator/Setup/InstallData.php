c1db8d2cb8e604d541a879258838680d0576ccdd
conghoatranvn <conghoaz243>
2016-12-12 20:10:13 +0700

    Custom Estimate validate quantity

--- app/code/FME/Pricecalculator/Setup/InstallData.php
+++ app/code/FME/Pricecalculator/Setup/InstallData.php
@@ -20,8 +20,9 @@ class InstallData implements InstallDataInterface {
      */
     private $eavSetupFactory;
 
-     /**
-     * InstallData constructor.
+    /**
+     * Init
+     *
      * @param EavSetupFactory $eavSetupFactory
      */
     public function __construct(EavSetupFactory $eavSetupFactory) {
@@ -29,8 +30,8 @@ class InstallData implements InstallDataInterface {
     }
 
     /**
-     * @param ModuleDataSetupInterface $setup
-     * @param ModuleContextInterface $context
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context) {
         /** @var EavSetup $eavSetup */
@@ -47,7 +48,7 @@ class InstallData implements InstallDataInterface {
             'visible' => true,
             'required' => false,
             'user_defined' => false,
-            'default' => 'discount=0,0;size=0,0;area;fixed',
+            'default' => '',
             'apply_to' => 'simple,configurable,bundle,grouped',
             'visible_on_front' => true,
             'is_configurable' => false,
@@ -56,8 +57,12 @@ class InstallData implements InstallDataInterface {
             'is_html_allowed_on_front' => true,
             'group' => 'Price Calculator',
             'sort_order' => '83',
-            'note' => "Default: discount=0,0;size=0,0;area;fixed <br>For area & discount in percentage: discount=10,50;size=15,30;area;percent
-                <br>For volume & discount in fixed: discount=10,50;size=15,30;volume;fixed",
+            'note' => "For area & discount in percentage  use below syntax "
+            . "discount=10,50;size=15,30;area;percent "
+            . "For volume & discount in fixed use below syntax "
+            . "discount=10,50;size=15,30;volume;fixed "
+            . "Discount is not tied with area or volume, you can use any type or do not give discount by specifing only. "
+            . "area or  volume in text field",
         ];
 
         $attributes['pricing_limit'] = [
@@ -69,7 +74,7 @@ class InstallData implements InstallDataInterface {
             'visible' => true,
             'required' => false,
             'user_defined' => false,
-            'default' => 'Width_min=0;Width_max=0;Height_min=0;Height_max=0',
+            'default' => '',
             'apply_to' => 'simple,configurable,bundle,grouped',
             'visible_on_front' => true,
             'is_configurable' => false,
@@ -78,9 +83,10 @@ class InstallData implements InstallDataInterface {
             'is_html_allowed_on_front' => true,
             'group' => 'Price Calculator',
             'sort_order' => '84',
-            'note' => "Default: Width_min=0;Width_max=0;Height_min=0;Height_max=0"
-                . "<br>For applying limit on custom options: Width_min=24;Width_max=1800;Height_min=50;Height_max=100"
-                . "<br>Please use keyword given in configuration like keyword_min or keyword_max",
+            'note' => "For applying limit on custom options please use the following syntax "
+            . "Length_min=5;Length_max=70;Width_min=5;Width_max=80 "
+            . "this will set upper and lower limit for each field "
+            . "Please use keyword given in configuration like keyword_min or keyword_max",
         ];
 
         $attributes['current_unit'] = [
@@ -92,7 +98,7 @@ class InstallData implements InstallDataInterface {
             'visible' => true,
             'required' => false,
             'user_defined' => false,
-            'default' => 'Inch',
+            'default' => '',
             'apply_to' => 'simple,configurable,bundle,grouped',
             'option' => [
                 'value' => [
@@ -189,9 +195,9 @@ class InstallData implements InstallDataInterface {
          * Add attributes to the eav/attribute
          */
         foreach ($attributes as $attribute => $options) {
+
             $eavSetup->addAttribute(\Magento\Catalog\Model\Product::ENTITY, $attribute, $options);
         }
-
 //        $eavSetup->addAttribute(\Magento\Catalog\Model\Product::ENTITY, 'test_attribute', [
 //            'type' => 'int',
 //            'backend' => '',
