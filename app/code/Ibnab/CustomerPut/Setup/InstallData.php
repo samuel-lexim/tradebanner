6062de9a131f8ce5c8ac54ac21841ccf3d42caa2
letunhatkong <letunhatkong@gmail.com>
2017-01-04 18:05:30 +0700

    register file

--- app/code/Ibnab/CustomerPut/Setup/InstallData.php
+++ app/code/Ibnab/CustomerPut/Setup/InstallData.php
@@ -39,9 +39,15 @@ class InstallData implements InstallDataInterface
         $installer->startSetup();
 
         $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
-        $entityTypeId = $customerSetup->getEntityTypeId(\Magento\Customer\Model\Customer::ENTITY);
-        $customerSetup->removeAttribute(\Magento\Customer\Model\Customer::ENTITY, "free_delivery_kong");
 
+        $used_in_forms[] = "adminhtml_customer";
+        $used_in_forms[] = "checkout_register";
+        $used_in_forms[] = "customer_account_create";
+        $used_in_forms[] = "customer_account_edit";
+        $used_in_forms[] = "adminhtml_checkout";
+
+        // free_delivery
+        $customerSetup->removeAttribute(\Magento\Customer\Model\Customer::ENTITY, "free_delivery_kong");
         $customerSetup->addAttribute(\Magento\Customer\Model\Customer::ENTITY, "free_delivery_kong", array(
             "type" => "varchar",
             "backend" => "",
@@ -57,22 +63,98 @@ class InstallData implements InstallDataInterface
 
         ));
 
-        $promotion_code = $customerSetup->getAttribute(\Magento\Customer\Model\Customer::ENTITY, "free_delivery_kong");
-
-        $promotion_code = $customerSetup->getEavConfig()->getAttribute(\Magento\Customer\Model\Customer::ENTITY, 'free_delivery_kong');
-        $used_in_forms[] = "adminhtml_customer";
-        $used_in_forms[] = "checkout_register";
-        $used_in_forms[] = "customer_account_create";
-        $used_in_forms[] = "customer_account_edit";
-        $used_in_forms[] = "adminhtml_checkout";
-        $promotion_code->setData("used_in_forms", $used_in_forms)
+        $free_delivery = $customerSetup->getEavConfig()->getAttribute(\Magento\Customer\Model\Customer::ENTITY, 'free_delivery_kong');
+        $free_delivery->setData("used_in_forms", $used_in_forms)
             ->setData("is_used_for_customer_segment", true)
             ->setData("is_system", 0)
             ->setData("is_user_defined", 1)
             ->setData("is_visible", 1)
             ->setData("sort_order", 100);
 
-        $promotion_code->save();
+        $free_delivery->save();
+
+
+        // Website Url
+        $customerSetup->removeAttribute(\Magento\Customer\Model\Customer::ENTITY, "website_url");
+        $customerSetup->addAttribute(\Magento\Customer\Model\Customer::ENTITY, "website_url", array(
+            "type" => "varchar",
+            "backend" => "",
+            "label" => "Website Url",
+            "input" => "text",
+            "source" => "",
+            "visible" => true,
+            "required" => false,
+            "default" => "",
+            "frontend" => "",
+            "unique" => false,
+            "note" => ""
+
+        ));
+
+        $website_url = $customerSetup->getEavConfig()->getAttribute(\Magento\Customer\Model\Customer::ENTITY, 'website_url');
+        $website_url->setData("used_in_forms", $used_in_forms)
+            ->setData("is_used_for_customer_segment", true)
+            ->setData("is_system", 0)
+            ->setData("is_user_defined", 1)
+            ->setData("is_visible", 1)
+            ->setData("sort_order", 110);
+
+        $website_url->save();
+
+        // Seller Permit
+        $customerSetup->removeAttribute(\Magento\Customer\Model\Customer::ENTITY, "seller_permit");
+        $customerSetup->addAttribute(\Magento\Customer\Model\Customer::ENTITY, "seller_permit", array(
+            "type" => "varchar",
+            "backend" => "",
+            "label" => "Seller Permit",
+            "input" => "text",
+            "source" => "",
+            "visible" => true,
+            "required" => false,
+            "default" => "",
+            "frontend" => "",
+            "unique" => false,
+            "note" => ""
+
+        ));
+
+        $seller_permit = $customerSetup->getEavConfig()->getAttribute(\Magento\Customer\Model\Customer::ENTITY, 'seller_permit');
+        $seller_permit->setData("used_in_forms", $used_in_forms)
+            ->setData("is_used_for_customer_segment", true)
+            ->setData("is_system", 0)
+            ->setData("is_user_defined", 1)
+            ->setData("is_visible", 1)
+            ->setData("sort_order", 120);
+
+        $seller_permit->save();
+
+
+        // Optional Message
+        $customerSetup->removeAttribute(\Magento\Customer\Model\Customer::ENTITY, "optional_message");
+        $customerSetup->addAttribute(\Magento\Customer\Model\Customer::ENTITY, "optional_message", array(
+            "type" => "varchar",
+            "backend" => "",
+            "label" => "Optional Message",
+            "input" => "text",
+            "source" => "",
+            "visible" => true,
+            "required" => false,
+            "default" => "",
+            "frontend" => "",
+            "unique" => false,
+            "note" => ""
+
+        ));
+
+        $optional_message = $customerSetup->getEavConfig()->getAttribute(\Magento\Customer\Model\Customer::ENTITY, 'optional_message');
+        $optional_message->setData("used_in_forms", $used_in_forms)
+            ->setData("is_used_for_customer_segment", true)
+            ->setData("is_system", 0)
+            ->setData("is_user_defined", 1)
+            ->setData("is_visible", 1)
+            ->setData("sort_order", 120);
+
+        $optional_message->save();
 
         $installer->endSetup();
     }
---
+++ app/code/Lexim/Override/Controller/Account/CreatePost.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * @author Samuel Kong
+ */
+
+namespace Lexim\Override\Controller\Account;
+
+use Magento\Customer\Model\Account\Redirect as AccountRedirect;
+use Magento\Customer\Api\Data\AddressInterface;
+use Magento\Framework\Api\DataObjectHelper;
+use Magento\Framework\App\Action\Context;
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Customer\Api\AccountManagementInterface;
+use Magento\Customer\Helper\Address;
+use Magento\Framework\UrlFactory;
+use Magento\Customer\Model\Metadata\FormFactory;
+use Magento\Newsletter\Model\SubscriberFactory;
+use Magento\Customer\Api\Data\RegionInterfaceFactory;
+use Magento\Customer\Api\Data\AddressInterfaceFactory;
+use Magento\Customer\Api\Data\CustomerInterfaceFactory;
+use Magento\Customer\Model\Url as CustomerUrl;
+use Magento\Customer\Model\Registration;
+use Magento\Framework\Escaper;
+use Magento\Customer\Model\CustomerExtractor;
+use Magento\Framework\Exception\StateException;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\File\UploaderFactory;
+use Magento\Framework\Filesystem;
+
+/**
+ * Class CreatePost
+ * @package Lexim\Override\Controller\Account
+ */
+class CreatePost extends \Magento\Customer\Controller\Account\CreatePost
+{
+
+    protected $fileUploaderFactory;
+    protected $fileSystem;
+
+    public function __construct(
+        Context $context,
+        Session $customerSession,
+        ScopeConfigInterface $scopeConfig,
+        StoreManagerInterface $storeManager,
+        AccountManagementInterface $accountManagement,
+        Address $addressHelper,
+        UrlFactory $urlFactory,
+        FormFactory $formFactory,
+        SubscriberFactory $subscriberFactory,
+        RegionInterfaceFactory $regionDataFactory,
+        AddressInterfaceFactory $addressDataFactory,
+        CustomerInterfaceFactory $customerDataFactory,
+        CustomerUrl $customerUrl,
+        Registration $registration,
+        Escaper $escape,
+        CustomerExtractor $customerExtractor,
+        DataObjectHelper $dataObjectHelper,
+        AccountRedirect $accountRedirect,
+        UploaderFactory $fileUploaderFactory,
+        Filesystem $fileSystem
+    )
+    {
+        $this->fileUploaderFactory = $fileUploaderFactory;
+        $this->fileSystem = $fileSystem;
+        parent::__construct($context, $customerSession, $scopeConfig, $storeManager, $accountManagement, $addressHelper, $urlFactory, $formFactory, $subscriberFactory, $regionDataFactory, $addressDataFactory, $customerDataFactory, $customerUrl, $registration, $escape, $customerExtractor, $dataObjectHelper, $accountRedirect);
+    }
+
+    /**
+     * @return \Magento\Framework\Controller\Result\Redirect
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        if ($this->session->isLoggedIn() || !$this->registration->isAllowed()) {
+            $resultRedirect->setPath('*/*/');
+            return $resultRedirect;
+        }
+
+        if (!$this->getRequest()->isPost()) {
+            $url = $this->urlModel->getUrl('*/*/create', ['_secure' => true]);
+            $resultRedirect->setUrl($this->_redirect->error($url));
+            return $resultRedirect;
+        }
+
+        $this->session->regenerateId();
+
+        try {
+            $address = $this->extractAddress();
+            $addresses = $address === null ? [] : [$address];
+
+            $customer = $this->customerExtractor->extract('customer_account_create', $this->_request);
+            $customer->setAddresses($addresses);
+
+            $password = $this->getRequest()->getParam('password');
+            $confirmation = $this->getRequest()->getParam('password_confirmation');
+            $redirectUrl = $this->session->getBeforeAuthUrl();
+
+            $this->checkPasswordConfirmation($password, $confirmation);
+
+            $customer = $this->accountManagement
+                ->createAccount($customer, $password, $redirectUrl);
+
+            // Upload File
+            $uploader = $this->fileUploaderFactory->create(['fileId' => 'seller_file']);
+            $uploader->setAllowedExtensions(['jpg', 'jpeg', 'pdf', 'png']);
+            $uploader->setAllowRenameFiles(true);
+            $uploader->setFilesDispersion(false);
+            $uploader->setAllowCreateFolders(true);
+
+            $path = $this->fileSystem->getDirectoryRead(DirectoryList::MEDIA)
+                ->getAbsolutePath('images/certificate/');
+
+            $result = $uploader->save($path);
+            if ($result) $customer->setCustomAttribute('seller_permit', $this->_url->getBaseUrl() . 'pub/media/images/certificate/' .  $result['file']);
+            // # Upload File
+
+
+            if ($this->getRequest()->getParam('is_subscribed', false)) {
+                $this->subscriberFactory->create()->subscribeCustomerById($customer->getId());
+            }
+
+            $this->_eventManager->dispatch(
+                'customer_register_success',
+                ['account_controller' => $this, 'customer' => $customer]
+            );
+
+            $confirmationStatus = $this->accountManagement->getConfirmationStatus($customer->getId());
+            if ($confirmationStatus === AccountManagementInterface::ACCOUNT_CONFIRMATION_REQUIRED) {
+                $email = $this->customerUrl->getEmailConfirmationUrl($customer->getEmail());
+                // @codingStandardsIgnoreStart
+                $this->messageManager->addSuccess(
+                    __(
+                        'You must confirm your account. Please check your email for the confirmation link or <a href="%1">click here</a> for a new link.',
+                        $email
+                    )
+                );
+                // @codingStandardsIgnoreEnd
+                $url = $this->urlModel->getUrl('*/*/index', ['_secure' => true]);
+                $resultRedirect->setUrl($this->_redirect->success($url));
+            } else {
+                $this->session->setCustomerDataAsLoggedIn($customer);
+                $this->messageManager->addSuccess($this->getSuccessMessage());
+                $resultRedirect = $this->accountRedirect->getRedirect();
+            }
+            return $resultRedirect;
+        } catch (StateException $e) {
+            $url = $this->urlModel->getUrl('customer/account/forgotpassword');
+            // @codingStandardsIgnoreStart
+            $message = __(
+                'There is already an account with this email address. If you are sure that it is your email address, <a href="%1">click here</a> to get your password and access your account.',
+                $url
+            );
+            // @codingStandardsIgnoreEnd
+            $this->messageManager->addError($message);
+        } catch (InputException $e) {
+            $this->messageManager->addError($this->escaper->escapeHtml($e->getMessage()));
+            foreach ($e->getErrors() as $error) {
+                $this->messageManager->addError($this->escaper->escapeHtml($error->getMessage()));
+            }
+        } catch (\Exception $e) {
+            $this->messageManager->addException($e, __('We can\'t save the customer.'));
+        }
+
+        $this->session->setCustomerFormData($this->getRequest()->getPostValue());
+        $defaultUrl = $this->urlModel->getUrl('*/*/create', ['_secure' => true]);
+        $resultRedirect->setUrl($this->_redirect->error($defaultUrl));
+        return $resultRedirect;
+    }
+
+
+
+
+}
